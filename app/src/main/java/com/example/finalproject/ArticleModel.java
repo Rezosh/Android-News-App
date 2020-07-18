441fab71ba1744c05b10daf910bebf5912bcfc7f
Rezosh <corneausebastien09@gmail.com>
2020-07-17 21:04:31 -0400

    Main Page 70% Complete

---
+++ app/src/main/java/com/example/finalproject/ArticleModel.java
@@ -0,0 +1,34 @@
+package com.example.finalproject;
+
+public class ArticleModel {
+
+    private String title;
+    private String url;
+    private String thumbnail;
+    private String section;
+
+
+    public ArticleModel(String title, String url, String thumbnail, String section) {
+        this.title = title;
+        this.url = url;
+        this.thumbnail = thumbnail;
+        this.section = section;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String getThumbnail() {
+        return thumbnail;
+    }
+
+    public String getSection() {
+        return section;
+    }
+}
